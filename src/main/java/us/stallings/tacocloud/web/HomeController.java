dd21c65032b715b085a7f24e325cfc01bd2a48dc
mstallings31 <matt@stallings.us>
2022-03-09 14:26:11 -0600

    Implemented page/model/controller to design tacos.

--- src/main/java/us/stallings/tacocloud/web/HomeController.java
+++ src/main/java/us/stallings/tacocloud/web/HomeController.java
@@ -1,4 +1,4 @@
-package us.stallings.tacocloud;
+package us.stallings.tacocloud.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/us/stallings/tacocloud/HomeControllerTest.java
+++ src/test/java/us/stallings/tacocloud/HomeControllerTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import us.stallings.tacocloud.web.HomeController;
+
 @WebMvcTest(HomeController.class) public class HomeControllerTest {
 
     @Autowired
