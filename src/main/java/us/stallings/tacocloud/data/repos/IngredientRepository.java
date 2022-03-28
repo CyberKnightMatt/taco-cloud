da9bf6874ddf4b24971c9517168c5dafcceebc1e
mstallings31 <matt@stallings.us>
2022-03-28 16:40:41 -0500

    Spring Data JDBC implementation.

--- src/main/java/us/stallings/tacocloud/data/repos/IngredientRepository.java
+++ src/main/java/us/stallings/tacocloud/data/repos/IngredientRepository.java
@@ -1,14 +1,11 @@
 package us.stallings.tacocloud.data.repos;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 import us.stallings.tacocloud.data.models.Ingredient;
 
 import java.util.Optional;
 
 
-public interface IngredientRepository {
-    Iterable<Ingredient> findAll();
-
-    Optional<Ingredient> findById(String id);
-
-    Ingredient save(Ingredient ingredient);
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 }
--- src/main/java/us/stallings/tacocloud/data/repos/OrderRepository.java
+++ src/main/java/us/stallings/tacocloud/data/repos/OrderRepository.java
@@ -1,10 +1,10 @@
 package us.stallings.tacocloud.data.repos;
 
+import org.springframework.data.repository.CrudRepository;
 import us.stallings.tacocloud.data.models.TacoOrder;
 
 import java.util.Optional;
 
 
-public interface OrderRepository {
-    TacoOrder save(TacoOrder order);
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 }
